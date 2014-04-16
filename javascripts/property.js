60bbafbc001a1101f29c75565da96e3a59aee6a3
Chermaine Zimmerman <C14jcdj@yahoo.com>
2014-04-15 18:49:14 -0700

    drop down menu doesn't stay open

--- javascripts/property.js
+++ javascripts/property.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
-    // rotateBackground();
+    rotateBackground();
+    $('#products').bind('mouseover', openSubMenu);
+    $('#products').bind('mouseout', closeSubMenu);
+
+    function openSubMenu() {
+        $('.submenu').css('visibility', 'visible');
+    };
+
+    function closeSubMenu() {
+        $('.submenu').css('visibility', 'hidden');
+    };
 })
 
-function Picture(image, text) {
+function Picture(image, text, id) {
     this.image = "img/" + image + "";
     this.textBig = text[0];
     this.textLittle = text[1];
+    this.id = id
 }
 
 function test() {
@@ -15,19 +26,27 @@ function test() {
 function rotateBackground() {
 
     var images = [
-        new Picture('commercialipadmini.jpg', ['Connect apps, and go mobile', 'Track deals in progress, sales, investors all with your mobile device']),
-        new Picture('developergrass.jpg', ['Track your sales cycle from pre-sale to closing', 'Manage inventory, get a birds-eye view of your sales team']),
-        new Picture('residential.jpg', ['Software for your real estate buisness', 'Track leads and referrals, market to your prospects'])
+        new Picture('commercialipadmini.jpg', ['Connect apps, and go mobile', 'Track deals in progress, sales, investors all with your mobile device'], '2'),
+        new Picture('developergrass.jpg', ['Track your sales cycle from pre-sale to closing', 'Manage inventory, get a birds-eye view of your sales team'], '3'),
+        new Picture('residential.jpg', ['Software for your real estate buisness', 'Track leads and referrals, market to your prospects'], '1')
     ];
     var counter = 0;
     var breaker = 0;
 
+    function changeLinks() {
+        var prevlink = (images[counter].id == "1") ? 3 : parseInt(images[counter].id) - 1;
+        $('.pictures').css("background", "url(" + images[counter].image + ")");
+        $("#" + prevlink + "").css('text-decoration', 'none');
+        $("#" + images[counter].id + "").css('text-decoration', 'underline');
+    };
+
     var rotator = function() {
-        $('.pictures').css("background", "url(" + images[counter].image + ")")
-        $('.pictures p:nth-child(1)').text(images[counter].textBig)
-        $('.pictures p:nth-child(2)').text(images[counter].textLittle)
+        changeLinks();
+        $('.pictures p:nth-child(1)').text(images[counter].textBig);
+        $('.pictures p:nth-child(2)').text(images[counter].textLittle);
         counter = (counter == images.length - 1) ? 0 : counter + 1;
     };
-    setInterval(rotator, 3000);
 
+
+    setInterval(rotator, 3000);
 }
